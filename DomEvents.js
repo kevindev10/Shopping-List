97653ec26ef1a2f2add2ce9e55c5ac85e41299f3
kevindev10 <kevindev1@outlook.com>
2020-03-09 16:54:17 +0300

    addition npm files

--- DomEvents.js
+++ DomEvents.js
@@ -1,3 +1,11 @@
+//import {without} from 'lodash';
+var _ = require('lodash');
+//console.log (without);
+//console.log(_);
+var array = [1,2,3,4,5,6,7,8,9];
+console.log('answer:',_.without(array,6));
+
+
 var input =document.getElementById("userinput");
 var button =document.getElementById("enter");
 var ul  =document.querySelector("ul");
